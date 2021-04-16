1274f32dde70c0366c00f7f0dad67c205ec5885b
edksam <62302678+edksam@users.noreply.github.com>
2021-04-16 15:08:08 +0100

    Initial Commit

---
+++ src/com/capgemini/Main.java
@@ -0,0 +1,14 @@
+package com.capgemini;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int x = 1, y=1, z=0;
+	if (x==y | x < ++y) {
+	    z = x+y;
+    } else {
+	    z=1;
+    }
+        System.out.println(z);
+    }
+}
